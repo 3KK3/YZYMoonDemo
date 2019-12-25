f98ee0c5edacc82e70cfe046c932b3eafade6817
芝麻酱 <yangzhiyong@huantengsmart.com>
2019-12-25 12:18:43 +0800

    Initial Commit

---
+++ YZYMoonDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YZYMoonDemo
+//
+//  Created by 芝麻酱 on 2019/12/25.
+//  Copyright © 2019 芝麻酱. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YZYMoonDemoTests/YZYMoonDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  YZYMoonDemoTests.swift
+//  YZYMoonDemoTests
+//
+//  Created by 芝麻酱 on 2019/12/25.
+//  Copyright © 2019 芝麻酱. All rights reserved.
+//
+
+import XCTest
+@testable import YZYMoonDemo
+
+class YZYMoonDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
